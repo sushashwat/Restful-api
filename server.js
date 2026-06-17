978936d52035841b060f5fc1dd934ba66a652cee
Shashwat Gupta <shashwat@gmail.com>
2026-06-17 14:28:13 +0530

    Delete user with id and Error Handlers

--- server.js
+++ server.js
@@ -67,3 +67,26 @@ app.put("/user/:id", validateUser, (req,res)=>{
     users[index] = {id: req.params.id, firstName, lastName, hobby};
     res.status(200).json({message: "User updated successfully", user: users[index]});
 });
+
+// DELETE /user/:id - delete user by id
+app.delete("/user/:id", (req,res)=> {
+    const index = users.findIndex((u)=> u.id === req.params.id);
+    if(index === -1){
+        return res.status(404).json({error: `User with id ${re.params.id} not found.`});
+    }
+    const deleteUser = users.splice(index,1)[0];
+    res.status(200).json({message:"User deleted successfully", user:deletedUser});
+});
+
+// ERROR HANDLERS
+
+//404 - route not found 
+app.use((req,res)=>{
+    res.status(404).json({error: `Route $(req.method) ${req.url} not found.`});
+});
+
+// 500 - unexpected server error 
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.status(500).json({error: "Interval Server Error. "});
+});
